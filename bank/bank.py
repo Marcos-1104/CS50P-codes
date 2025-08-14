a578fd6113ad15bdf29d31133ed266ef0e9b9e9b
Marcos Alejandro Puig Cancanon <138168769+Marcos-1104@users.noreply.github.com>
2025-08-14 13:30:04 -0300

    Add files via upload

---
+++ bank/bank.py
@@ -0,0 +1,11 @@
+def main():
+    greeting = input("Greeting:\n").strip().lower()
+
+    if greeting[:5] == "hello":
+        print("$0")
+    elif greeting[:1] == "h":
+        print("$20")
+    else:
+        print("$100")
+
+main()
